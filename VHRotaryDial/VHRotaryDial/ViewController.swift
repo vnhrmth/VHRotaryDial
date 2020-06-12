e873bfeefa95ac23fd3b2be28cc68e63590bf701
Vinay <vnhrmth@gmail.com>
2020-06-12 13:19:50 +0530

    Initial Commit of project structure.

---
+++ VHRotaryDial/VHRotaryDial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VHRotaryDial
+//
+//  Created by AudiologyMacbookPro4 on 12/06/20.
+//  Copyright © 2020 com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
